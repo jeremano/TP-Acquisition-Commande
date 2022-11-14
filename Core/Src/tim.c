4228f79687184875f28a0b64e204d25413c2f7ed
AnthonyBOUDET <94923796+AnthonyBOUDET@users.noreply.github.com>
2022-11-14 15:26:03 +0100

    isoreset alpha

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -60,7 +60,7 @@ void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 3994;
+  sConfigOC.Pulse = 2663;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
@@ -70,7 +70,6 @@ void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sConfigOC.Pulse = 1331;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
